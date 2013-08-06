bac73303f3b9114f43c34c0e6a5155c34760424d
Nero Sule <sulenero@gmail.com>
2013-08-06 09:10:52 +0300

    Console logging works.

---
+++ Tests/WhenConsoleAppenderLoggerIsCalled.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLogger;
+using NLogger.Configuration;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Tests.Helpers;
+
+namespace Tests
+{
+    [TestFixture]
+    public class WhenConsoleAppenderLoggerIsCalled
+    {
+        private ILogger _logger;
+
+        #region Configuration file string
+
+        private const string Configuration =
+@"<NLoggerConfiguration>
+  <root>
+    <level error=""true""/>
+  </root>
+  <appender name=""ConsoleAppender"" type=""NLogger.Appenders.ConsoleLoggerAppender"" parameters=""C:\BS\log.txt"">
+    <level info=""true""/>
+    <pattern value=""%date %level %message""/>
+  </appender>
+</NLoggerConfiguration>";
+
+        #endregion
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new Logger();
+            _logger.Initialize(new NLoggerConfigurationSectionXmlLoader(Configuration));
+        }
+
+        [Test]
+        public void ConsoleOutputShouldIncrease()
+        {
+            var current = Console.Out;
+            const string text = "Outputting info";
+            using (var output = new ConsoleOutput())
+            {
+                _logger.LogInfo(text);
+                Assert.That(output.GetOutput(), Is.StringContaining(text));
+            }
+            Assert.That(Console.Out, Is.EqualTo(current));
+        }
+    }
+}
